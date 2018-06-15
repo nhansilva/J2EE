681bcfaf79e2e51fdf1cceb4a4802f84422e2e84
nhansilva <vtnhanit95@gmail.com>
2018-06-15 19:38:36 +0700

    commit controller

--- src/main/java/vn/com/phongvucrawler/Controller/HomeController.java
+++ src/main/java/vn/com/phongvucrawler/Controller/HomeController.java
@@ -1,18 +1,21 @@
 package vn.com.phongvucrawler.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.com.phongvucrawler.Repository.PVDataRepository;
+
 @Controller
 public class HomeController {
-//	@Autowired
-//	PVDataRepository data;
+	@Autowired
+	PVDataRepository data;
 
 	@RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
 	public String Home(Model model) {
-//		model.addAttribute("datalist", data.findAll());
+		model.addAttribute("datalist", data.findAll());
 		return "index";
 	}
 	
--- src/main/java/vn/com/phongvucrawler/Repository/PVDataRepository.java
+++ src/main/java/vn/com/phongvucrawler/Repository/PVDataRepository.java
@@ -1,9 +1,10 @@
 package vn.com.phongvucrawler.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
 import vn.com.phongvucrawler.Entity.ProductInformation;
 
 public interface PVDataRepository extends MongoRepository<ProductInformation, String>  {
-	ProductInformation findProductInfoation(String productId);
+	
 }
