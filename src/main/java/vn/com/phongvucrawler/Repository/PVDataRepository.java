9239bc5c1b5b5c5560fdcca592a09e176a806a7c
nhansilva <vtnhanit95@gmail.com>
2018-06-15 19:39:23 +0700

    commit

--- src/main/java/vn/com/phongvucrawler/Repository/PVDataRepository.java
+++ src/main/java/vn/com/phongvucrawler/Repository/PVDataRepository.java
@@ -1,7 +1,6 @@
 package vn.com.phongvucrawler.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
 import vn.com.phongvucrawler.Entity.ProductInformation;
 
